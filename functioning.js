93a33038109c1808f9f0b913884c91c49be494c3
Finn-Reinders <finn.reinders@sintlucasedu.nl>
2025-05-17 13:35:53 +0200

    Menu items toegevoegd

--- functioning.js
+++ functioning.js
@@ -3,28 +3,31 @@ document.addEventListener("DOMContentLoaded", function()
     const navbar = document.getElementById('navbar');
     const logo = document.getElementById('logo');
     const bracket = document.getElementById('bracket');
-
+    const menuItems = document.querySelectorAll('.menu-item');
+    
     // wanneer de pagina geladen is komt de animatie van de navbar en logo
     navbar.style.animation = 'navSlide 2s ease-in-out forwards';
     logo.style.animation = 'logoToNav 3s ease-in-out forwards';
-
+    
     navbar.addEventListener('animationend', function() {
         // hier komen moet de opacity denk ik omhoog van de keyboards
     });
-
+    
     let menuSwitch = false;
-    logo.addEventListener('animationend', function() {
-        logo.addEventListener('mouseenter', function() {
-            logo.style.transform = 'scale(1.1)';
-        });
-
+    logo.addEventListener('animationend', function() {     
         logo.addEventListener('click', function() {
             menuSwitch = !menuSwitch;
             if (menuSwitch) {
-            bracket.classList.add("open");
+                bracket.classList.add("open");
+                menuItems.forEach(item => {
+                    item.classList.add("opened");
+                });
             }
             else {
                 bracket.classList.remove("open");
+                menuItems.forEach(item => {
+                    item.classList.remove("opened");
+                });
             }
             console.log('clicked');
         });
